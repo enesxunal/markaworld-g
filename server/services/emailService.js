c4740edad6b1d5afb709a1f95c8d8ed972cdde38
enesxunal <enesunal700@gmail.com>
2025-07-08 16:56:15 +0300

    mail onay

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,6 +2,8 @@ const { google } = require('googleapis');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+const FRONTEND_URL = 'https://markaworld.com.tr';
+
 const oAuth2Client = new google.auth.OAuth2(
   process.env.GMAIL_CLIENT_ID,
   process.env.GMAIL_CLIENT_SECRET,
@@ -31,4 +33,11 @@ async function sendMail(to, subject, html) {
   });
 }
 
-module.exports = { sendMail };
+async function sendCustomerRegistrationEmail(customer, verificationToken) {
+  const verificationLink = `${FRONTEND_URL}/verify-email/${verificationToken}`;
+  const subject = 'Marka World Hesabınızı Aktifleştirin';
+  const html = `<h2>Merhaba ${customer.name},</h2><p>Marka World'e hoşgeldiniz!</p><p>Hesabınızı aktifleştirmek için aşağıdaki bağlantıya tıklayınız:</p><a href="${verificationLink}" style="display:inline-block;padding:12px 24px;background:#000;color:#fff;text-decoration:none;border-radius:6px;font-weight:bold;margin:16px 0;">E-posta Adresimi Doğrula</a><p>Eğer butona tıklayamazsanız, aşağıdaki bağlantıyı kopyalayıp tarayıcınızda açabilirsiniz:</p><p><a href="${verificationLink}">${verificationLink}</a></p><br><p>Saygılarımızla,<br>Marka World</p>`;
+  await sendMail(customer.email, subject, html);
+}
+
+module.exports = { sendMail, sendCustomerRegistrationEmail, FRONTEND_URL };
